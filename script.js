2302602239da504d859b19edb9ce470104b92587
msouza-dev <msouza@email.rs>
2025-11-19 10:11:41 -0300

    Adicionado uma função

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function marcarGol() {
-    
+function marcarGol(time) {
+    const container = document.getElementById("gols" + time);
+
+    const bolinha = document.createElement("div");
+    bolinha.classList.add("bolinha");
+
+    container.appendChild(bolinha);
 }
